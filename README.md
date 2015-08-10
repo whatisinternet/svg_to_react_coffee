1e731e79cb1d2017b252d8a4b903f20f73d21625
Whatisinternet <joshteeter@gmail.com>
2015-08-09 20:36:03 -0400

    First fully working pass. Still needs a lot of TLC and refactoring.

---
+++ README.md
@@ -0,0 +1,44 @@
+# SVG to React Coffee
+
+Converts SVG images into valid(ish) React SVG components that can be easily
+embeded into react render functions.
+
+-----
+
+## TODO:
+- Write to something other than std::out
+- Wrap in React.CreateFactory React.CreateClass and a render: ->
+
+## Installation:
+
+### Compile yourself:
+
+1. 
+  Install [Rust and cargo](http://doc.crates.io/)
+2.
+  ```shell
+    git clone git@github.com:whatisinternet/svg_to_react_coffee.git
+  ```
+3.
+  ```shell
+    cd svg_to_react_coffee && cargo build --release
+  ```
+4.
+  You can find the executable in target/release
+
+### Ruby gem:
+  Coming soon!
+
+## Usage
+  Print to screen:
+    ./svg_to_react_coffee file.svg
+  Write to file:
+    ./svg_to_react_coffee file.svg > output.coffee
+
+## Contributing
+
+1. Fork it ( https://github.com/whatisinternet/svg_to_react_coffee/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate xml;
+extern crate case;
 
 use std::fs::File;
 use std::io::BufReader;
 
 use xml::reader::EventReader;
 use xml::reader::events::*;
+
+use case::CaseExt;
 
 use std::env;
 
@@ -20,29 +23,74 @@ fn parse_off_extra_w3c_details (input_string: String) -> String{
     return format!("{}", string_vector[1]);
 }
 
+fn tab_in(depth: usize) -> String {
+    let mut tabs = "".to_string();
+    for i in (1..).take(depth){
+        tabs = format!("{}  ",&tabs);
+    }
+    return format!("{}",&tabs);
+}
+
+fn transform_attribute (attribute: String) -> String {
+    let attribute_key_value: Vec<&str> = attribute.split("=").collect();
+    let attribute_key: &str = attribute_key_value[0];
+    let attribute_value: &str = attribute_key_value[1];
+    return formatted_attribute(attribute_key, attribute_value);
+}
+
+fn formatted_attribute ( attribute_key: &str, attribute_value: &str) -> String {
+    let mut attributes = "".to_string();
+    if attribute_key.contains(":"){
+        attributes = format!("\"{}\": {}",attribute_key.to_string().to_camel_lowercase(),attribute_value);
+    }
+    else{
+        attributes = format!("{}: {}",attribute_key.to_camel_lowercase(),attribute_value);
+    }
+    return attributes.to_string();
+}
+
+fn build_attributes(attributes: Vec<xml::attribute::OwnedAttribute>, depth: usize) {
+    for attribute in attributes{
+        let temp_attribute: String = format!("{}", attribute);
+        let parsed_attribute: String = parse_off_extra_w3c_details(temp_attribute);
+        println!("{}{}", tab_in(depth + 1), transform_attribute(parsed_attribute));
+    }
+}
+
+fn build_element(name: xml::name::OwnedName, attributes: Vec<xml::attribute::OwnedAttribute>, depth: usize) {
+    let temp_name: String = format!("{}", name);
+    let svg_tag: String = parse_off_extra_w3c_details(temp_name);
+    if valid_react_dom_element(&svg_tag) {
+        println!("{}{}", tab_in(depth), svg_tag);
+        build_attributes(attributes.clone(), depth);
+    }
+}
+
+fn get_file(file_name: String) -> BufReader<std::fs::File>{
+    let full_file_path_and_name = format!("{}/{}",env::current_dir().unwrap().display(),file_name);
+    let file = File::open(full_file_path_and_name).unwrap();
+    return  BufReader::new(file);
+}
+
+
 fn parse_svg (file_name: String){
 
-  let full_file_path_and_name = format!("{}/{}",env::current_dir().unwrap().display(),file_name);
-  println!("{}",full_file_path_and_name);
-  let file = File::open(full_file_path_and_name).unwrap();
-  let file = BufReader::new(file);
+    let file = get_file(file_name);
+    let mut parser = EventReader::new(file);
 
-  let mut parser = EventReader::new(file);
-  let mut depth = 0;
-  for e in parser.events() {
-    match e {
-      XmlEvent::StartElement { name, attributes, .. } => {
-        for attribute in attributes{
-            let temp_name: String = format!("{}", name);
-            let temp_attribute: String = format!("{}", attribute);
-            let svg_tag: String = parse_off_extra_w3c_details(temp_name);
-            let parsed_attribute: String = parse_off_extra_w3c_details(temp_attribute);
-            println!("{} + {}      {}",  svg_tag, valid_react_dom_element(&svg_tag), parsed_attribute);
+    let mut depth = 0;
+    for e in parser.events() {
+        match e {
+            XmlEvent::StartElement { name, attributes, .. } => {
+                build_element(name, attributes, depth);
+                depth += 1;
+            }
+            XmlEvent::EndElement { .. } => {
+                depth -= 1;
+            }
+            _ => {}
         }
-      }
-      _ => {}
     }
-  }
 }
 
 
