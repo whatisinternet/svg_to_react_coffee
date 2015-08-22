67ba0620062c023715f24913249a93244555417c
Whatisinternet <joshteeter@gmail.com>
2015-08-21 21:57:12 -0400

    :rocket: Started converting all println in favour of using a Vector that will get passed back.

--- lib/lib.rs
+++ lib/lib.rs
@@ -9,8 +9,8 @@ use parser::parser::*;
 #[no_mangle]
 pub extern "C" fn convert(svg_file_name: String) {
   let file_name: Vec<&str> = svg_file_name.split(".svg").collect();
-  println!("module.exports = React.createFactory React.createClass\n\n  render: ->");
+  let mut header_string: String = format!("module.exports = React.createFactory React.createClass\n\n  render: ->");
   if file_name.len() > 1 {
-    parse_svg(svg_file_name.clone());
+    parse_svg(svg_file_name.clone(), header_string.clone());
   }
 }
--- lib/parser/parser.rs
+++ lib/parser/parser.rs
@@ -7,17 +7,23 @@ use xml::reader::EventReader;
 use xml::reader::events::*;
 
 
-pub fn parse_svg (file_name: String){
+pub fn parse_svg (file_name: String, header_string: String) -> Vec<String>{
 
     let file = get_file(file_name);
+    let output_vector: Vec<String> = vec!();
     let mut parser = EventReader::new(file);
+
+    // let mut output_vector: Vec<String> = [""];
 
     let mut depth = 2;
     let mut was_valid_element = false;
+
     for e in parser.events() {
         match e {
             XmlEvent::StartElement { name, attributes, .. } => {
-                was_valid_element = build_element(name, attributes, depth);
+                let mut element_vector: Vec<String> = build_element(name, attributes, depth);
+                output_vector.push_all(element_vector.to_vec());
+                was_valid_element = is_valid_element(name);
                 depth += 1;
             }
             XmlEvent::EndElement { .. } => {
@@ -35,4 +41,5 @@ pub fn parse_svg (file_name: String){
             _ => {}
         }
     }
+    return output_vector;
 }
