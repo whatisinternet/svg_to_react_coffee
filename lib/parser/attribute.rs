5322f62d9a1e6258c721990d24972624efb7c290
whatisinternet <joshteeter@gmail.com>
2015-08-24 22:34:44 -0400

    Added testing for all classes and fixed up any small bugs along the way. Extracted any methods that had gotten too long or complex. The project should have a fairly high test coverage now.
    Full test suite for util :+1:. It needs a refactor though.
    
    Moved tests out into a module.
    
    Started tests for element. Fixed up parser to only pass a string for
    element name.
    
    Added further tests for attributes.
    
    Adding more tests to attribute. Still a long way to go.
    
    Added tests for dealing with generic attributes. Still need a null case
    test. Further needs more tests for attributes.
    
    Completed tests for more of attributes.
    
    Only missing a few more tests for 100% on attribute :+1:
    
    Added full coverage to attributes portion of the lib
    
    Extracted some of the methods from the parser out. Some more extration might be a good idea, but overall it's not too bad

--- lib/parser/attribute.rs
+++ lib/parser/attribute.rs
@@ -7,7 +7,7 @@ use parser::util::parse_off_extra_w3c_details;
 use parser::util::valid_react_attribute;
 
 pub fn build_attributes(
-    attributes: Vec<xml::attribute::OwnedAttribute>,
+    attributes: Vec<String>,
     depth: usize) -> Vec<String> {
 
     let mut element_attributes: Vec<String> = vec!();
@@ -19,7 +19,7 @@ pub fn build_attributes(
     return element_attributes;
 }
 
-    fn valid_attribute( depth: usize, attribute: xml::attribute::OwnedAttribute) -> String {
+    fn valid_attribute( depth: usize, attribute: String) -> String {
         let temp_attribute: String = format!("{}", attribute);
         let parsed_attribute: String = parse_off_extra_w3c_details(temp_attribute);
         let transformed_attribute: String = format!("{}",transform_attribute(parsed_attribute));
@@ -42,7 +42,7 @@ pub fn build_attributes(
                 let temp_attribute = attribute_key.replace("-","_").to_camel_lowercase().to_string();
                 if valid_react_attribute(&temp_attribute) {
                     return parse_attribute(attribute_key, attribute_value);
-            }
+                }
                 return "".to_string();
             }
 
@@ -75,7 +75,7 @@ pub fn build_attributes(
                                             attribute_value);
                     }
                         fn parse_attribute_value ( attribute_value: &str) -> String {
-                            return attribute_value
+                            return format!("{}", attribute_value
                                 .replace(",","")
                                 .replace("-","_")
                                 .replace("\"","")
@@ -83,11 +83,152 @@ pub fn build_attributes(
                                 .replace(";","\", ")
                                 .trim_right_matches("\", ")
                                 .to_string()
-                                .to_camel_lowercase();
+                                .to_camel_lowercase());
                         }
 
+#[test]
+fn it_should_build_a_vector_of_valid_attributes() {
+    let mock_depth: usize = 0;
+    let mock_attribute: String = "{w3c}width=baz".to_string();
+    let mock_attributes: Vec<String> = vec!(mock_attribute);
+    let expected_string: String = "    width: baz".to_string();
+    let expected_vector: Vec<String> = vec!(expected_string);
+    let asserted_vector: Vec<String> = build_attributes(mock_attributes, mock_depth);
 
+    for n in 0..(asserted_vector.len() - 1)  {
+        assert_eq!( asserted_vector[n], expected_vector[n]);
+    }
+}
 
+#[test]
+fn it_should_return_an_empty_string_from_valid_attribute_when_given_invalid_data() {
+    let mock_depth: usize = 0;
+    let mock_attribute: String = "{w3c}foo=baz".to_string();
+    let expected_string: String = "".to_string();
+    let asserted_string: String = valid_attribute(mock_depth, mock_attribute);
 
+    assert!( asserted_string == expected_string);
+}
 
+#[test]
+fn it_should_return_a_properly_formatted_attribute_from_valid_attribute() {
+    let mock_depth: usize = 0;
+    let mock_attribute: String = "{w3c}width=baz".to_string();
+    let expected_string: String = "    width: baz".to_string();
+    let asserted_string: String = valid_attribute(mock_depth, mock_attribute);
 
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_a_valid_attribute_when_a_valid_attribute_transform() {
+    let mock_attribute: String = "width=baz".to_string();
+    let expected_string: String = "width: baz".to_string();
+    let asserted_string: String = transform_attribute(mock_attribute);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_nothing_when_an_invalid_attribute_when_given_a_valid_attribute_transform() {
+    let mock_attribute: String = "foo=baz".to_string();
+    let expected_string: String = "".to_string();
+    let asserted_string: String = transform_attribute(mock_attribute);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_nothing_when_an_invalid_attribute_when_given_a_valid_attribute() {
+    let mock_key: &str = "foo";
+    let mock_value: &str = "baz";
+    let expected_string: String = "".to_string();
+    let asserted_string: String = formatted_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_formatted_attribute_when_given_a_valid_attribute() {
+    let mock_key: &str = "width";
+    let mock_value: &str = "baz";
+    let expected_string: String = "width: baz".to_string();
+    let asserted_string: String = formatted_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_nothing_on_invalid_attribute_non_style() {
+    let mock_key: &str = "foo-bar:";
+    let mock_value: &str = "baz";
+    let expected_string: String = "".to_string();
+    let asserted_string: String = formatted_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_a_parsed_attribute_style() {
+    let mock_key: &str = "style";
+    let mock_value: &str = "bar-baz:\"baz-foo\";";
+    let expected_string: String = "style: { barBaz: \"bazFoo\" }".to_string();
+    let asserted_string: String = parse_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_a_parsed_attribute_non_style() {
+    let mock_key: &str = "foo-bar";
+    let mock_value: &str = "baz";
+    let expected_string: String = "fooBar: baz".to_string();
+    let asserted_string: String = parse_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_true_on_valid_style_key() {
+    let mock_key: &str = "style";
+    let asserted_bool: bool = is_style_attribute(mock_key);
+
+    assert!( asserted_bool == true);
+}
+
+#[test]
+fn it_should_return_false_on_fake_style_key() {
+    let mock_key: &str = "foo-bar";
+    let asserted_bool: bool = is_style_attribute(mock_key);
+
+    assert!( asserted_bool == false);
+}
+
+#[test]
+fn it_should_return_a_parsed_style_attribute() {
+    let mock_key: &str = "foo-bar";
+    let mock_value: &str = "bar-baz:\"baz-foo\";";
+    let expected_string: String = "fooBar: { barBaz: \"bazFoo\" }".to_string();
+    let asserted_string: String = parse_style_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_a_parsed_key_value_attribute() {
+    let mock_key: &str = "foo-bar";
+    let mock_value: &str = "baz";
+    let expected_string: String = "fooBar: baz".to_string();
+    let asserted_string: String = parse_normal_attribute(mock_key, mock_value);
+
+    assert!( asserted_string == expected_string);
+}
+
+#[test]
+fn it_should_return_a_parsed_attribute() {
+    let mock_string: &str = "foo-bar:\"baz\";";
+    let expected_string: String = "fooBar: \"baz".to_string();
+    let asserted_string: String = parse_attribute_value(mock_string);
+
+    assert!( asserted_string == expected_string);
+}
--- lib/parser/parser.rs
+++ lib/parser/parser.rs
@@ -21,11 +21,17 @@ pub fn parse_svg (file_name: String, header_string: String) -> Vec<String>{
     for e in parser.events() {
         match e {
             XmlEvent::StartElement { name, attributes, .. } => {
-                let element_vector: Vec<String> = build_element(name.clone(), attributes, depth);
-                for completed_element in element_vector {
-                    output_vector.push(completed_element.clone());
+                let temp_name: String = format!("{}", name.clone());
+                let mut extracted_attributes: Vec<String> = vec!();
+                for attribute in attributes {
+                    extracted_attributes.push(format!("{}", attribute));
                 }
-                was_valid_element = is_valid_element(name.clone());
+                for start_element_and_attribute in parse_start_element(temp_name.clone(),
+                                                                       extracted_attributes,
+                                                                       depth){
+                    output_vector.push(start_element_and_attribute)
+                }
+                was_valid_element = is_valid_element(temp_name.clone());
                 depth += 1;
             }
             XmlEvent::EndElement { .. } => {
@@ -34,11 +40,7 @@ pub fn parse_svg (file_name: String, header_string: String) -> Vec<String>{
             XmlEvent::Characters(data) => {
                 if was_valid_element {
                     depth += 1;
-                    if !data.contains(">") {
-                        let valid_data: String = format!("{}\"{}\"\n\n", tab_in(depth), data.to_string());
-                        output_vector.push(valid_data);
-
-                    }
+                    output_vector.push(parse_character(data, depth));
                     depth -= 1;
                 }
             }
@@ -47,3 +49,23 @@ pub fn parse_svg (file_name: String, header_string: String) -> Vec<String>{
     }
     return output_vector;
 }
+    fn parse_start_element( name: String,
+                            attributes: Vec<String>,
+                            depth: usize ) -> Vec<String> {
+
+        let mut output_vector: Vec<String> = vec!();
+        let element_vector: Vec<String> =
+            build_element(name.clone(), attributes, depth);
+
+        for completed_element in element_vector {
+            output_vector.push(completed_element.clone());
+        }
+        return output_vector;
+    }
+
+    fn parse_character(data: String ,depth: usize) -> String {
+        if !data.contains(">") {
+            return format!("{}\"{}\"\n\n", tab_in(depth), data.to_string());
+        }
+        return "".to_string();
+    }
